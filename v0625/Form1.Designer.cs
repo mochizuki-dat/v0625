e1870da6011e8cc704836fd1da5b40f3a27ec308
kai-machida <49702001+kai-machida@users.noreply.github.com>
2020-06-25 11:58:49 +0900

    ボタンを消す

--- v0625/Form1.Designer.cs
+++ v0625/Form1.Designer.cs
@@ -28,25 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.button100 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // button100
             // 
-            this.button1.Location = new System.Drawing.Point(12, 58);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 120);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button100.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button100.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button100.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button100.Location = new System.Drawing.Point(103, 95);
+            this.button100.Name = "button100";
+            this.button100.Size = new System.Drawing.Size(134, 142);
+            this.button100.TabIndex = 0;
+            this.button100.Text = "button1";
+            this.button100.UseVisualStyleBackColor = false;
+            this.button100.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button100);
+            this.Enabled = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +60,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button100;
     }
 }
 
